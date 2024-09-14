69fbbcd4778d4a5a45e4613f2c8231efaacb3291
GregorMorningstar <gregormorningstar@gmail.com>
2024-09-14 17:43:47 +0200

    dodano widoki do admina i funkcje logout

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -10,4 +11,17 @@ class AdminController extends Controller
     {
         return view('admin.index');
     }
+
+    public function AdminLogOut(Request $request)
+    {
+
+        //dd($request);
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+
+
 }
